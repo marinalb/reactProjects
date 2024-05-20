de1bc3349fd2971f8aca7ca8d154104a122537e5
mbarros@national.aaa.com <marina.barros@acad.pucrs.br>
2024-05-20 19:41:59 -0300

    Module 1 commit

---
+++ index.js
@@ -0,0 +1,22 @@
+document.body.innerHTML = `
+<h1>Hello World!</h1>
+<h2>Hellou World!</h2>
+<h3>Hellou World!</h3>
+<h4>Hellou World!</h4>
+<h5>Hellou World!</h5>
+<h6>Hellou World!</h6>
+`;
+
+document.getElementById("app").innerHTML = `
+<h1>Hello World!</h1>
+`;
+
+document.getElementById("demo").innerHTML = `
+<h2>Hellou World!</h2>
+`;
+document.getElementById("test").innerHTML = `
+<h4>Hellou World!</h4>
+`;
+document.getElementById("module").innerHTML = `
+<h6>Hellou World!</h6>
+`;
