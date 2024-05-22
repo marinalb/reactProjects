73c7619cc961c9df923af47c2c03e1f404f6f371
mbarros@national.aaa.com <marina.barros@acad.pucrs.br>
2024-05-22 17:55:07 -0300

    Fixing module1

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-document.body.innerHTML = `
-<h1>Hello World!</h1>
-<h2>Hellou World!</h2>
-<h3>Hellou World!</h3>
-<h4>Hellou World!</h4>
-<h5>Hellou World!</h5>
-<h6>Hellou World!</h6>
-`;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello World!</h1>
-`;
+document.body.insertAdjacentHTML ("afterbegin",
+"<h1>Hello World!</h1><h2>Hellou World!</h2><h3>Hellou World!</h3><h4>Hellou World!</h4><h5>Hellou World!</h5><h6>Hellou World!</h6>");
+
+document.getElementById("app").insertAdjacentHTML ("afterbegin", "<h6>Hellou World!</h6>");
 
 document.getElementById("demo").innerHTML = `
-<h2>Hellou World!</h2>
+<p>Hellou World!<p>
 `;
 document.getElementById("test").innerHTML = `
 <h4>Hellou World!</h4>
